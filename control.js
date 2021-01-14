6c632d0fe1e0097df7ff95dcc5d0269c5c08e93b
Bernard Telles <bernardtelgon@gmail.com>
2021-01-13 23:36:25 -0300

    control finance

---
+++ control.js
@@ -0,0 +1,7 @@
+const dummyTransactions = [
+  { id: 1, name: 'Bolo de brigadeiro', amount: -20},
+  { id: 2, name: 'Salário', amount: 300},
+  { id: 3, name: 'Torto de Frango', amount: -10 }
+  { id: 4, name: 'violao', amount: 150 }
+  ]
+
