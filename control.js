c940a70f2c494c0b89e18eb710806414c9d2abb4
Bernard Telles <bernardtelgon@gmail.com>
2021-01-19 23:57:31 -0300

    builting control

--- control.js
+++ control.js
@@ -1,9 +1,13 @@
 const dummyTransactions = [
-  { id: 1, name: 'Bolo de brigadeiro', amount: -20},
-  { id: 2, name: 'Salário', amount: 300},
-  { id: 3, name: 'Torto de Frango', amount: -10 }
-  { id: 4, name: 'violao', amount: 150 }
-  ]
+  {  id: 1, name: 'Bolo de brigadeiro', amount: -20},
+  {  id: 2, name: 'Salário', amount: 300},
+  {  id: 3, name: 'Torto de Frango', amount: -10 }
+  {  id: 4, name: 'violao', amount: 150 }
+]
+
+const addTrasactionIntoDom = trasaction => {
+  
+}
 
 
   
