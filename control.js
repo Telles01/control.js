f2753323e4cc7238aa6ac7e4bd5dadca8f068d31
Bernard Telles <bernardtelgon@gmail.com>
2021-02-12 23:46:16 -0300

    builting control

--- control.js
+++ control.js
@@ -1,8 +1,9 @@
 const dummyTransactions = [
   {  id: 1, name: 'Bolo de brigadeiro', amount: -20},
   {  id: 2, name: 'Salário', amount: 300},
-  {  id: 3, name: 'Torto de Frango', amount: -10 }
+  {  id: 3, name: 'Torta de Frango', amount: -10 }
   {  id: 4, name: 'violao', amount: 150 }
+  {  id: 5, name: 'ice cream', amount: 150 }
 ]
 
 
